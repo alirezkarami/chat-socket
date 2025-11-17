fb22a27945ee0d25bc651fe328d7a9013826fcfa
Your Name <you@example.com>
2025-11-17 12:26:06 +0330

    add socker details

--- chat_sock/asgi.py
+++ chat_sock/asgi.py
@@ -1,17 +1,13 @@
-"""
-ASGI config for chat_sock project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
+import django
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+import chat.routing  # این خیلی مهمه
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_sock.settings')
+
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
